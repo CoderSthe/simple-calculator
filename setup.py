9876596a13a64cd3b3c52c57d6abfbea64a0bc6c
Coder Sthe <shotleftdriver@gmail.com>
2022-09-05 13:44:58 +0200

    Updates url address

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='Simple Calculator',
     long_description=open('README.md').read(),
-    url='https://github.com/CoderSthe/<package-name>',
+    url='https://github.com/CoderSthe/simple-calculator',
     author='Sithembiso Mdhluli',
     author_email='mdhluli269@gmail.com'
 )
